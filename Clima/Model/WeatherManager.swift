8bfcf7ab37407cfaad4e423774d4e08c8901256a
Rafael Fernandez <myztajay@hotmail.com>
2022-05-17 19:31:01 -0400

    moved relevant logic into weather model and added some computed vars

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -44,10 +44,17 @@ struct WeatherManager {
         do {
             // decoder takes itself as an argument
             let decodedData =  try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            
+            let id  = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            // instantiate weather model
+            let weatherModel = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weatherModel.conditionName)
         } catch {
             print(error)
         }
         
     }
+    
 }
