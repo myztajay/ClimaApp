7cfdf66793b7351434f351cfa5f3c052b7ceb295
Rafael Fernandez <myztajay@hotmail.com>
2022-05-17 16:31:29 -0400

    added search UI functionality

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Allows delegate functionality in this controller
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        var input = searchTextField.text
+        searchTextField.endEditing(true)
+        print(input!)
+    }
+    // delegate action
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    // a check of wether or
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return searchTextField.text != ""
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
