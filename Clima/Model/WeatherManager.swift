1079cc2461a7f710d4ba1333aebcfb84c1a3d71e
Rafael Fernandez <myztajay@hotmail.com>
2022-05-17 19:04:41 -0400

    updated struct to include more data, mainly description for future display of description

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -29,12 +29,25 @@ struct WeatherManager {
                 }
                 // printing response if we have one
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
+                    parseJSON(weatherData: safeData)
                 }
             }
             // all tasks start suspended
             task.resume()
         }
+    }
+    
+    func parseJSON (weatherData: Data){
+        // choose decoder
+        let decoder = JSONDecoder()
+        // do try blocks and error handling
+        do {
+            // decoder takes itself as an argument
+            let decodedData =  try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+        
     }
 }
